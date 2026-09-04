966993918f913a67a988ba048fe57de04df95ac8
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2026-09-04 21:11:58 +0200

    RTC_added

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,64 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <RTClib.h>
 #include "LargeLCD.h"
 
 //#define LED_PIN 13
 
 //LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 LargeLCD largeLcd(12, 11, 10, 9, 8, 7);
+RTC_DS3231 rtc;
 
 char msg[17]; //  buffer for messsages
+int lastMinute = -1;
+int lastSecond = -1;
 
 void setup() {
   Serial.begin(9600);
   largeLcd.begin();
+
+  if (!rtc.begin()) {
+    largeLcd.printString("RTC Error!");
+    while (1);
+  }
+
+  // Set the RTC to the time the code was compiled if it lost power
+  if (rtc.lostPower()) {
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+  
   largeLcd.clear();
 }
 
 void loop() {
+  DateTime now = rtc.now();
 
-  // for (int h = 1; h < 21; h+=3){
-  //   for (int m = 1; m <= 21; m+=3) {
-  //     largeLcd.bigNumber(h, 0, 2);
-  //     largeLcd.separator2(7);
-  //     largeLcd.bigNumber(m, 9, 2, true);
-  //     delay(500);
-  //     largeLcd.sp2(7);
-  //     delay(500);
-  //   }
-  // }
-  
-  largeLcd.clear();
-  delay(200);
-  char msg[17];
-  int hour = 12;
-  int minute = 7;
-  int sec = 34;
-
-  largeLcd.printString("current time is");
+  // Update Hours and Minutes only when the minute changes
+  if (now.minute() != lastMinute) {
+    lastMinute = now.minute();
     
-  snprintf(msg, sizeof(msg), "%02d:%02d:%02d" , hour, minute, sec);
-  largeLcd.printString(msg, 1, 4);
-
-  delay(1000);
+    // Print HH at col 0, fixed 2-digit length, padded with '0'
+    largeLcd.bigNumber(now.hour(), 0, 2, true);
     
+    // Print MM at col 9, fixed 2-digit length, padded with '0'
+    largeLcd.bigNumber(now.minute(), 9, 2, true);
+  }
+
+  // Handle the blinking colon every second
+  if (now.second() != lastSecond) {
+    lastSecond = now.second();
+    
+    if (lastSecond % 2 == 0) {
+      // Draw colon at col 6
+      largeLcd.separator2(7);
+    } else {
+      // Erase colon by printing 3 blank spaces over it on both rows
+      largeLcd.printString("  ", 0, 7);
+      largeLcd.printString("  ", 1, 7);
+    }
+  }
+
+  // Small delay to prevent loop from running unnecessarily fast
+  delay(100);
 }
